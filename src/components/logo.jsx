413971aacead4298a762e5f9d0c1c4d472c5a652
Cachumbin <simoncarreno2006@gmail.com>
2024-12-24 18:30:52 -0500

    feat: Add basic styles to logo

--- src/components/logo.jsx
+++ src/components/logo.jsx
@@ -2,9 +2,9 @@ import Logo from "../assets/Recurso 1.png";
 
 const logo = () => {
   return (
-    <div>
-      <h1>CachunFood</h1>
-      <img src={Logo} alt="" />
+    <div className="logoContainer">
+      <h1 className="logoText">CachunFood</h1>
+      <img src={Logo} alt="" className="logoImg" />
     </div>
   );
 };

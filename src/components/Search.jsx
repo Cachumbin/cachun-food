f74488c32ef93c8d19d9c5893453fb65347eabd5
Cachumbin <simoncarreno2006@gmail.com>
2025-01-08 14:55:22 -0500

    feat: Add input and debounce function to search component

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,7 +1,24 @@
+import { useState } from "react";
+
 const Search = () => {
+  const [search, setSearch] = useState("");
+
+  const debounce = (fetchingFunction, delay) => {
+    let timeout;
+    return (...args) => {
+      clearTimeout(timeout);
+    };
+  };
+
   return (
     <div>
-      <input type="text" />
+      <input
+        type="text"
+        value={search}
+        onChange={(e) => {
+          setSearch(e.target.value);
+        }}
+      />
     </div>
   );
 };

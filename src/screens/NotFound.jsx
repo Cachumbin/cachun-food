8c0825bfdd2d194e8260a0b5f33e694bbb61c002
Cachumbin <simoncarreno2006@gmail.com>
2025-02-09 15:23:40 -0500

    feat: Add 404 window

--- src/screens/NotFound.jsx
+++ src/screens/NotFound.jsx
@@ -1,10 +1,24 @@
 import Logo from "../components/logo";
+import { IoClose } from "react-icons/io5";
 
 const NotFound = () => {
   return (
     <div>
       <Logo />
-      <p>Not Found</p>
+      <div className="window">
+        <div className="window-top-bar">
+          <button className="window-button">
+            <IoClose className="window-icon" />
+          </button>
+          <div className="dialog-text-container window-text-container">
+            <p className="window-text">Not Found</p>
+          </div>
+        </div>
+        <div className="dialog-content window-content">
+          <h1>Error 404</h1>
+          <p>Page not found</p>
+        </div>
+      </div>
     </div>
   );
 };

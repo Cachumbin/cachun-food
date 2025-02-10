9fc116f3ef61ca3e6c0999c488b0360bac2029ff
Cachumbin <simoncarreno2006@gmail.com>
2025-02-10 15:23:40 -0500

    feat: Add 404 page styles

--- src/screens/NotFound.jsx
+++ src/screens/NotFound.jsx
@@ -1,24 +1,40 @@
 import Logo from "../components/logo";
 import { IoClose } from "react-icons/io5";
+import Construction from "../assets/construction.png";
+import Tape from "../assets/yellow-tape.png";
 
 const NotFound = () => {
   return (
-    <div>
-      <Logo />
-      <div className="window">
-        <div className="window-top-bar">
-          <button className="window-button">
-            <IoClose className="window-icon" />
-          </button>
-          <div className="dialog-text-container window-text-container">
-            <p className="window-text">Not Found</p>
+    <div className="home">
+      <section className="header">
+        <Logo />
+      </section>
+      <main className="main-container">
+        <div className="window error-404-window">
+          <div className="window-top-bar error-404-top-bar">
+            <button className="window-button error-404-button">
+              <IoClose className="window-icon" />
+            </button>
+            <div className="dialog-text-container window-text-container error-404-text-container">
+              <p className="window-text">Not Found</p>
+            </div>
+          </div>
+          <div className="dialog-content window-content error-404-content">
+            <h1>Error 404</h1>
+            <p>Page not found</p>
+            <div>
+              <p>
+                The page you are looking for might have been removed, had its
+                name changed or is temporarily unavailable.
+              </p>
+            </div>
+            <div className="superposed-container">
+              <img src={Tape} alt="Tape" />
+              <img src={Construction} alt="Construction" />
+            </div>
           </div>
         </div>
-        <div className="dialog-content window-content">
-          <h1>Error 404</h1>
-          <p>Page not found</p>
-        </div>
-      </div>
+      </main>
     </div>
   );
 };

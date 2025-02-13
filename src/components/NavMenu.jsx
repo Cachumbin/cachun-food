329b659ee5bfd0fa3ac568c7e39811c2f5a4f115
Cachumbin <simoncarreno2006@gmail.com>
2025-02-13 15:23:40 -0500

    feat: Create nav menu

---
+++ src/components/NavMenu.jsx
@@ -0,0 +1,65 @@
+import { useEffect, useState } from "react";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
+import SignIn from "./SignIn";
+import SignOut from "./SignOut";
+import { IoClose } from "react-icons/io5";
+
+const NavMenu = () => {
+  const [user, setUser] = useState(null);
+  const [userDetails, setUserDetails] = useState(null);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const auth = getAuth();
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setUser(user);
+      if (user) {
+        setUserDetails({
+          displayName: user.displayName,
+          email: user.email,
+          photoURL: user.photoURL,
+        });
+      } else {
+        setUserDetails(null);
+      }
+    });
+    return () => unsubscribe();
+  }, []);
+
+  const goToHome = () => {
+    navigate("/");
+  };
+
+  const goToSaved = () => {
+    navigate("/saved");
+  };
+
+  const goToSearch = () => {
+    navigate("/search");
+  };
+
+  return (
+    <div className="navmenu-window window">
+      <div className="navmenu-top-bar window-top-bar">
+        <button className="navmenu-button window-button">
+          <IoClose className="navmenu-icon window-icon" />
+        </button>
+        <div className="navmenu-text-container window-text-container">
+          <p className="window-text">Logo</p>
+        </div>
+      </div>
+      <div className="navmenu-content window-content">
+        <div className="window-content-page">
+          {user ? <SignOut info={userDetails} /> : <SignIn />}
+          <button onClick={goToHome}>Home</button>
+          <button onClick={goToSaved}>Saved</button>
+
+          <button onClick={goToSearch}>Search</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NavMenu;
--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,11 +1,13 @@
 import Logo from "../components/logo";
 import Discover from "../components/Discover";
+import NavMenu from "../components/NavMenu";
 
 const Home = () => {
   return (
     <div className="home">
       <section className="header">
         <Logo />
+        <NavMenu />
       </section>
       <main className="main-container">
         <Discover />
